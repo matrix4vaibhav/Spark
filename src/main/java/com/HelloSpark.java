f0899304a82465bcad43ec4b4106afabf38f0fbf
Vaibhav Goel <vagoel66@in.ibm.com>
2020-09-14 12:26:49 +0530

    modified

--- src/main/java/com/HelloSpark.java
+++ src/main/java/com/HelloSpark.java
@@ -4,8 +4,11 @@ import static spark.Spark.*;
 
 public class HelloSpark {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {	
 		get("/hello", (req, res) -> "Hello Spark!!!");
+		get("/hello/:name", (request, response) -> {
+		    return "Hello: " + request.params(":name");
+		});
 	}
 
 }
